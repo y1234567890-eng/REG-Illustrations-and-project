883dee2febd64b577dbe38c1673584171c0a8910
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-11-18 00:06:51 +0300

    Added Splash Scrren

---
+++ REGIllustrations&Project/app/src/main/java/com/yousufjamil/regillustrationsproject/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.yousufjamil.regillustrationsproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+
+@Suppress("DEPRECATION")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val splashLaunchAppName: LinearLayout = findViewById(R.id.startupHolderLayout)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        splashLaunchAppName.startAnimation(slideAnimation)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
