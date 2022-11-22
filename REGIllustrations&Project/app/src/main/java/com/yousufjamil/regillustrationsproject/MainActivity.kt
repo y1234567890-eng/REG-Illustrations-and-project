724a86632e4403f5b04a770a680857ab8705e364
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-11-22 22:08:42 +0300

    Updated look

--- REGIllustrations&Project/app/src/main/java/com/yousufjamil/regillustrationsproject/MainActivity.kt
+++ REGIllustrations&Project/app/src/main/java/com/yousufjamil/regillustrationsproject/MainActivity.kt
@@ -3,14 +3,14 @@ package com.yousufjamil.regillustrationsproject
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.TextView
 
@@ -21,22 +21,49 @@ class MainActivity : AppCompatActivity() {
         val webView: WebView = findViewById(R.id.webView)
         val webViewName: TextView = findViewById(R.id.webpageNameTxt)
         val loadProgress: ProgressBar = findViewById(R.id.loadProgress)
+//        val refreshWebBtn: ImageButton = findViewById(R.id.refreshImgBtn)
+        val homeWebBtn: ImageButton = findViewById(R.id.homeImgBtn)
 
+        homeWebBtn.visibility = View.INVISIBLE
         loadProgress.visibility = View.INVISIBLE
         webView.loadUrl("https://science-pro.repl.co/")
+//        webView.loadUrl("https://replit.com/")
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
         webView.canGoBack()
+
+//        refreshWebBtn.setOnClickListener {
+//            val currentUrl = webView.url
+//            if (currentUrl != null) {
+//                webView.loadUrl(currentUrl)
+//            }
+//        }
+
+        homeWebBtn.setOnClickListener {
+            webView.loadUrl("https://science-pro.repl.co/")
+            homeWebBtn.visibility = View.INVISIBLE
+        }
+
         webView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(
+            override fun onReceivedSslError(
                 view: WebView?,
-                request: WebResourceRequest?,
-                error: WebResourceError?
+                handler: SslErrorHandler?,
+                error: SslError?
             ) {
+                super.onReceivedSslError(view, handler, error)
                 val site = Intent(Intent.ACTION_VIEW)
                 site.data = Uri.parse("https://science-pro.repl.co/")
                 startActivity(site)
             }
+//            override fun onReceivedError(
+//                view: WebView?,
+//                request: WebResourceRequest?,
+//                error: WebResourceError?
+//            ) {
+//                val site = Intent(Intent.ACTION_VIEW)
+//                site.data = Uri.parse("https://science-pro.repl.co/")
+//                startActivity(site)
+//            }
 
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
@@ -49,6 +76,11 @@ class MainActivity : AppCompatActivity() {
                 loadProgress.visibility = View.INVISIBLE
                 webViewName.setText(view?.title)
                 webView.setOnTouchListener(View.OnTouchListener { v, event -> false })
+                if (webView.url == "https://science-pro.repl.co/") {
+                    homeWebBtn.visibility = View.INVISIBLE
+                } else {
+                    homeWebBtn.visibility = View.VISIBLE
+                }
             }
         }
     }
